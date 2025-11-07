aa44534ed6298985d587632c4cb2f945d163bc34
LeilaTEBROURI <leila.tebrouri@edu.univ-fcomte.fr>
2025-11-07 15:56:24 +0100

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const titre= document.querySelector("h1")
+
+titre.style.color="red"
+
+const bouton= document.querySelector("button");
+
+const paragrapheRouge= document.getElementById("a-mettre-en-rouge");
+
+paragrapheRouge.style.color="rouge"
+
