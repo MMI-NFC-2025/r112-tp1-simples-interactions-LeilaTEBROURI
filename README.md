35e9b34c076b25036d2e5cf985911d81090b6d29
LeilaTEBROURI <leila.tebrouri@edu.univ-fcomte.fr>
2025-11-07 16:27:46 +0100

    fin

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/LLH4HAdn)
 - nom : Tebrouri
 - prénom : Leila
-- URL Netlify :
+- URL Netlify : https://aquamarine-lily-2d9a7d.netlify.app/
 
 # Travail
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 // TODO
+const paragrapheRouge= document.getElementById("a-mettre-en-rouge");
+paragrapheRouge.style.color="red"
+
+const bouton = document.getElementById('en-rouge-suite-a-click');
+
+bouton.addEventListener('click', () => {
+
+  bouton.style.backgroundColor = "red";
+});
+
+
+const tousH2 = document.querySelectorAll('h2');
+
+
+tousH2.forEach(h2 => {
+  h2.addEventListener('click', (event) => {
+
+    event.target.style.color = 'red';
+  });
+});
